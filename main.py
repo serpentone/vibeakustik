7d7d2a979596d3d86e36a5f02ce92f3d3d9c33fc
serpentone <kivanckitapci@gmail.com>
2025-02-25 18:55:18 +0300

    Enemy added

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
-import math
+from random import randint
+import csv
+
+class Room:
+    def __init__(self):
+        self.size = int()
+        self.chest = False
+        self.enemyCount = int()
+        self.doorCount = int()
+        self.enemyDict = {}
+
+    def setRoom(self):
+        self.size = randint(1,4)
+        self.enemyCount = randint(1,3)
+        self.doorCount = randint(1,2)
+        chestRoll = randint(1,2)
+        if chestRoll == 1:
+            self.chest = True
+        else:
+            self.chest = False
+    
+    def setEnemyArr(self):
+        enemy = Enemy()
+        self.enemyDict.update[enemy]
+
+class Enemy:
+    def __init__(self, prefix, suffix):
+        self.name = prefix + " of the " + suffix
+        self.prefix = prefix
+        self.suffix = suffix
+        self.health = int()
+        self.damage = int()
+
+    def setEnemy(self):
+        pass
+
+room = Room()
+room.setRoom()
+print(room.size, room.chest, room.enemyCount, room.doorCount)
+enemy = Enemy("Warrior", "North")
+print(enemy.name)
